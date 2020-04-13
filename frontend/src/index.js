9a2d811268f5917bb6083939d2ec6e2546f217dc
Educhin <c.s.arinze@gmail.com>
2020-04-13 15:56:58 -0400

    set canvas method added to set canvas on dom load

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,13 @@ const width = canvas.width = window.innerWidth
 const height = canvas.height = window.innerHeight
 const ctx = canvas.getContext('2d')
 
-ctx.fillStyle = 'rgb(0, 0, 0)'
-ctx.fillRect(0, 0, width, height)
+document.addEventListener('DOMContentLoaded', function(){
+    setCanvas();
+    console.log('ran')
+});
+
+function setCanvas(){
+    ctx.fillStyle = 'rgb(0, 0, 0)'
+    ctx.fillRect(0, 0, width, height)
+    console.log('running')
+}
