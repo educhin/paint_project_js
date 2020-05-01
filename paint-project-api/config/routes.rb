10cff58a28dd240f48070272cec1ea56b9bbf1ac
Educhin <c.s.arinze@gmail.com>
2020-04-30 21:40:12 -0400

    clarifies exact routes being used

--- paint-project-api/config/routes.rb
+++ paint-project-api/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artists, only: [:index]
   resources :palettes, only: [:index]
-  resources :masterpieces
+  resources :masterpieces, only: [:index, :show, :create]
 end
